7d810e73b4b9180f8d672c0e532f3b5bb31028d8
Ferry To <ferry@verdantsparks.io>
2019-08-10 14:52:10 +0800

    fix typescript compiler error
    https://github.com/vuetifyjs/vuetify/issues/5944

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vuetify, {VCard, VBtn} from 'vuetify/lib';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  components: {
+    VCard, VBtn,
+  },
+});
 
 export default new Vuetify({
   icons: {
